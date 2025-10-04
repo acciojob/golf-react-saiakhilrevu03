1bce6fb1e0089947f39f79684304bd5a92e54287
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-04 23:26:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,60 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      ballVisible: false,
+      ballPosition: 0,
     };
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  handleStartClick = () => {
+    this.setState({ ballVisible: true });
+  };
+
+  handleKeyDown = (event) => {
+    if (!this.state.ballVisible) return;
+    if (event.keyCode === 39) { // Right arrow key
+      this.setState(prevState => ({
+        ballPosition: prevState.ballPosition + 5,
+      }));
     }
+  };
+
+  render() {
+    const { ballVisible, ballPosition } = this.state;
+
+    return (
+      <div>
+        {!ballVisible ? (
+          <button className="start" onClick={this.handleStartClick}>
+            Start
+          </button>
+        ) : (
+          <div
+            className="ball"
+            style={{
+              position: 'relative',
+              left: ballPosition,
+              width: 50,
+              height: 50,
+              borderRadius: '50%',
+              backgroundColor: 'green',
+            }}
+          />
+        )}
+      </div>
+    );
+  }
 }
-
 
 export default App;
